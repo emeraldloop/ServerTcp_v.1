c3d4fe39fb05c02124baa19e2372fa574d559cdd
emeraldloop <sergey.nikishin.2000@mail.ru>
2021-05-16 16:24:43 +0300

    Добавьте файлы проекта.

---
+++ ClientTcp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Введите сообщение: ");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
